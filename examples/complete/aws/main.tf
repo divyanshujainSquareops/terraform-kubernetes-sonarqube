0823b5b1a27fd7c87af806f4ceb5eecdeed94091
amanravi-squareops <aman.ravi@squareops.comm>
2025-05-20 15:04:37 +0530

    fixed output

--- examples/complete/aws/main.tf
+++ examples/complete/aws/main.tf
@@ -13,7 +13,7 @@ module "sonarqube" {
   source  = "squareops/sonarqube/kubernetes"
   version = "3.1.1"
   sonarqube_config = {
-    hostname                       = "sonarqube.squareops.in"
+    hostname                       = ""
     values_yaml                    = file("./helm/values.yaml")
     storage_class_name             = "gp2"
     sonarqube_volume_size          = "5Gi"
@@ -22,10 +22,10 @@ module "sonarqube" {
     monitoringPasscode             = ""
     postgresql_password_external   = ""
     postgresql_external_server_url = ""
-    sonarqube_password             = ""
+    sonarqube_password             = ""  #password must be 15 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character
 
     updateExistingSonarqube     = false # if you have existing sonarqube and want to upgrade,then enable it.
-    sonarqube_current_password  = "xxxxx" # if you upgrade sonarqube then you have to provide your previous sonarqube password ##Secret name=sonarqube-postgresql
-    postgresql_current_password = "xxxxxxx" # if you upgrade sonarqube then you have to provide your previous postgresql password ##Secret name=sonarqube-sonarqube-admin-password
+    sonarqube_current_password  = "" # if you upgrade sonarqube then you have to provide your previous sonarqube password ##Secret name=sonarqube-postgresql
+    postgresql_current_password = "" # if you upgrade sonarqube then you have to provide your previous postgresql password ##Secret name=sonarqube-sonarqube-admin-password
   }
 }
